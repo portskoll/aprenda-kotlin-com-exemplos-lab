9ecc29438601068d1c1fa676819b2d758b187fd5
MundoTela <portskoll@hotmail.com>
2024-01-26 19:48:52 -0300

    primeiro commit

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,48 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class NivelFormacao {BASICO, INTERMEDIARIO, AVANCADO}
+enum class NivelUsuarioIngles {BASICO, INTERMEDIARIO, AVANCADO}
+data class Usuario(val nome: String, val idade: Int, val ingles: NivelUsuarioIngles, val xp: Int)
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivelFormacao: NivelFormacao)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val aluno1 = Usuario("João", 22, NivelUsuarioIngles.BASICO, 3000)
+    val aluno2 = Usuario("Maria", 45, NivelUsuarioIngles.AVANCADO, 5000)
+    val aluno3 = Usuario("Henrique Portella", 52, NivelUsuarioIngles.AVANCADO, 20000)
+    val aluno4 = Usuario("Fabio Portella", 52, NivelUsuarioIngles.BASICO, 15000)
+
+
+    val conteudo1 = ConteudoEducacional("Programação em Kotlin", 120, NivelFormacao.AVANCADO )
+    val conteudo2 = ConteudoEducacional("Desenvolvimento Android", 180, NivelFormacao.INTERMEDIARIO)
+    val conteudo3 = ConteudoEducacional("Desenvolvimento Flask", 20, NivelFormacao.BASICO)
+    val conteudo4 = ConteudoEducacional("Desenvolvimento Python", 30, NivelFormacao.INTERMEDIARIO)
+
+
+
+    val formacao1 = Formacao("Desenvolvimento Mobile", listOf(conteudo1, conteudo2))
+    val formacao2 = Formacao("Python Backend", listOf(conteudo3, conteudo4))
+
+    formacao1.matricular(aluno1)
+    formacao1.matricular(aluno2)
+
+    formacao2.matricular(aluno3)
+    formacao2.matricular(aluno4)
+    println("Nossos Cursos:")
+    println()
+    println("Formação: ${formacao1.nome}")
+    println("Conteúdos: ${formacao1.conteudos.joinToString { it.nome + " ("+ it.nivelFormacao + ")"}}")
+    println("Alunos matriculados: ${formacao1.inscritos.joinToString { it.nome  + " " + it.idade + " anos - XP: " + it.xp}}")
+    println()
+    println("Formação: ${formacao2.nome}")
+    println("Conteúdos: ${formacao2.conteudos.joinToString { it.nome + " ("+ it.nivelFormacao + ")"}}")
+    println("Alunos matriculados: ${formacao2.inscritos.joinToString { it.nome  + " " + it.idade + " anos - XP: " + it.xp}}")
 }
